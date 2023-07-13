dc26be1c3aeaaea20b3a1a87d8af33c11cbd34b9
Callum McMahon <s4148161@terrace.qld.edu.au>
2023-07-14 08:54:04 +1000

    commit

---
+++ practice.py
@@ -0,0 +1,46 @@
+# python list
+
+fruitbasket = ['apple', 'banana', 'cherry', 'mango']
+""" print(fruitbasket[0])
+
+print(len(fruitbasket))
+
+for fruit in fruitbasket:
+    print(fruit)
+
+for i in range(len(fruitbasket)):
+    print(i)
+    print(fruitbasket[i]) """
+
+# python dictionary practice
+fruitdict = {
+    'apple':10,
+    'banana':3, 
+    'watermelon':1
+}
+
+fruitdict['coconut'] = 100
+
+for item in fruitdict:
+    print(fruitdict[item])
+
+dictlist = [
+    {
+        'item1': 1,
+        'item2': 2
+    },
+    {
+        'item3': 3,
+        'item4': 4
+    },
+]
+
+listdict = {
+    'line1': [1,2,3,4],
+    'line2': [5,6,7,8],
+    'line3': [9,10,11,12]
+}
+
+print(dictlist)
+print(listdict)
+
